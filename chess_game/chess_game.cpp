5fe7bf8531fdbfb46d496d46eac7205ab0e20775
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-08-19 13:18:56 +0400

    Update chess_game.cpp

--- chess_game/chess_game.cpp
+++ chess_game/chess_game.cpp
@@ -8,9 +8,7 @@ void  play_game(std::string** arr, int size);
 
 void printBoard()
 {
-
 	std::cout << "\t  Chess Game\n" << std::endl;
-	
 	int size = 8;
 	std::string** arr = new std::string*[size];
 	for(int i = 0; i < size;++i) {
@@ -23,24 +21,21 @@ void printBoard()
 	}
 
 	players(arr,size);
-
-
-
 }
 
 
 
 void players (std::string** arr , int size) {
 
-      	std::string king = "♔"; 
-        std::string queen = "♕";
+		std::string king = "♔"; 
+		std::string queen = "♕";
 		std::string rook = "♖";
 		std::string bishop = "♗";
 		std::string night = "♘";
 		std::string pawn = "♙";
 
-      	std::string King = "♚"; 
-        std::string Queen = "♛";
+		std::string King = "♚"; 
+		std::string Queen = "♛";
 		std::string Rook = "♜";
 		std::string Bishop = "♝";
 		std::string Night = "♞";
@@ -48,45 +43,45 @@ void players (std::string** arr , int size) {
 
 
 		arr[0][0] =  rook;
-       	arr[0][1] =  night; 
+		arr[0][1] =  night; 
 		arr[0][2] =  bishop;
-       	arr[0][3] =  queen;
-    	arr[0][4] =  king;
-       	arr[0][5] =  bishop;
+		arr[0][3] =  queen;
+		arr[0][4] =  king;
+		arr[0][5] =  bishop;
 		arr[0][6] =  night;
-       	arr[0][7] =  rook;
+		arr[0][7] =  rook;
 
 
-    	arr[1][0] =  pawn;
-       	arr[1][1] =  pawn; 
+		arr[1][0] =  pawn;
+		arr[1][1] =  pawn; 
 		arr[1][2] =  pawn;
-       	arr[1][3] =  pawn;
-    	arr[1][4] =  pawn;
-       	arr[1][5] =  pawn;
+		arr[1][3] =  pawn;
+		arr[1][4] =  pawn;
+		arr[1][5] =  pawn;
 		arr[1][6] =  pawn;
-       	arr[1][7] =  pawn;
+		arr[1][7] =  pawn;
 
 
 
 
-     	arr[7][0] =  Rook;
-       	arr[7][1] =  Night; 
+		arr[7][0] =  Rook;
+		arr[7][1] =  Night; 
 		arr[7][2] =  Bishop;
-       	arr[7][3] =  Queen;
-    	arr[7][4] =  King;
-       	arr[7][5] =  Bishop;
+		arr[7][3] =  Queen;
+		arr[7][4] =  King;
+		arr[7][5] =  Bishop;
 		arr[7][6] =  Night;
-       	arr[7][7] =  Rook;
+		arr[7][7] =  Rook;
 
 
-    	arr[6][0] =  Pawn;
-       	arr[6][1] =  Pawn; 
+		arr[6][0] =  Pawn;
+		arr[6][1] =  Pawn; 
 		arr[6][2] =  Pawn;
-       	arr[6][3] =  Pawn;
-    	arr[6][4] =  Pawn;
-       	arr[6][5] =  Pawn;
+		arr[6][3] =  Pawn;
+		arr[6][4] =  Pawn;
+		arr[6][5] =  Pawn;
 		arr[6][6] =  Pawn;
-       	arr[6][7] =  Pawn;
+		arr[6][7] =  Pawn;
 
 
 
@@ -101,15 +96,15 @@ void play_game(std::string** arr,int size) {
     std::cin >> no;
  do{
   
-    std::string king = "♔"; 
-    std::string queen = "♕";
+	        std::string king = "♔"; 
+		std::string queen = "♕";
 		std::string rook = "♖";
 		std::string bishop = "♗";
 		std::string night = "♘";
 		std::string pawn = "♙";
 
-    std::string King = "♚"; 
-    std::string Queen = "♛";
+	        std::string King = "♚"; 
+	        std::string Queen = "♛";
 		std::string Rook = "♜";
 		std::string Bishop = "♝";
 		std::string Night = "♞ ";
@@ -215,23 +210,21 @@ void play_game(std::string** arr,int size) {
 		}
 	}
    
-   if(( x == 7 ) && (y == 6)) {
+	   if(( x == 7 ) && (y == 6)) {
 		if(arr[change_x][change_y] == " "){
 		  		arr[x][y] = " ";
 	   arr[change_x][change_y] = Night;
 		}
 	}
 
-
-
-   if(( x == 0 ) && (y == 1)) {
+	   if(( x == 0 ) && (y == 1)) {
 		if(arr[change_x][change_y] == " "){
 		  		arr[x][y] = " ";
 	   arr[change_x][change_y] = night;
 		}
 	}
    
-   if(( x == 0) && (y == 6)) {
+	   if(( x == 0) && (y == 6)) {
 		if(arr[change_x][change_y] == " "){
 		  		arr[x][y] = " ";
 	   arr[change_x][change_y] = night;
@@ -239,8 +232,8 @@ void play_game(std::string** arr,int size) {
 	}
 /// final night
 
-    /// start King
- if(( x == 7) && (y == 4)) {
+	    /// start King
+	 if(( x == 7) && (y == 4)) {
 		
 		if(arr[change_x][change_y] == " "){
 		  arr[x][y] = " ";
@@ -379,9 +372,6 @@ void play_game(std::string** arr,int size) {
 		  }
 		}
 	}
-	
-	
-
 
 
 	for(int i = 0; i < size;++i) {
@@ -389,33 +379,24 @@ void play_game(std::string** arr,int size) {
 	   std::cout << arr[i][j] << " ";
 	 	}
 	 std::cout  << std::endl;
+	}	
+	}while(no != "n");
 	}
-}while(no != "n");
-}
 
 
 void gprint(std::string** arr,int size) {
-
-		for(int i = 0; i < size;++i) {
-		for(int  j = 0; j < size;++j) {
-		 std::cout << arr[i][j] << " " ;
-			}
-		std::cout << std::endl;
-		}
-
+    for(int i = 0; i < size;++i) {
+	for(int  j = 0; j < size;++j) {
+	 std::cout << arr[i][j] << " " ;
+	}
+	std::cout << std::endl;
+	}
 }
 
 int main()
 {
-
-	printBoard();
-
+    printBoard();
     return 0;
 }
-
-
-
-
-
 
 
